e4ffd9edb2032ad0eb79310c2b1575f47060d455
Andy Armstrong <andy@hexten.net>
2009-05-29 21:57:56 +0100

    Update build system

--- inc/MyBuilder.pm
+++ inc/MyBuilder.pm
@@ -1,8 +1,46 @@
 package MyBuilder;
 
-BEGIN {
-  require Module::Build;
-  @ISA = qw( Module::Build );
+use base qw( Module::Build );
+
+sub create_build_script {
+  my ( $self, @args ) = @_;
+  $self->_auto_mm;
+  return $self->SUPER::create_build_script( @args );
+}
+
+sub _auto_mm {
+  my $self = shift;
+  my $mm   = $self->meta_merge;
+  my @meta = qw( homepage bugtracker MailingList repository );
+  for my $meta ( @meta ) {
+    next if exists $mm->{resources}{$meta};
+    my $auto = "_auto_$meta";
+    next unless $self->can( $auto );
+    my $av = $self->$auto();
+    $mm->{resources}{$meta} = $av if defined $av;
+  }
+  $self->meta_merge( $mm );
+}
+
+sub _auto_repository {
+  my $self = shift;
+  if ( -d '.svn' ) {
+    my $info = `svn info .`;
+    return $1 if $info =~ /^URL:\s+(.+)$/m;
+  }
+  elsif ( -d '.git' ) {
+    my $info = `git remote -v`;
+    return unless $info =~ /^origin\s+(.+)$/m;
+    my $url = $1;
+    # Special case: patch up github URLs
+    $url =~ s!^git\@github\.com:!git://github.com/!;
+    return $url;
+  }
+  return;
+}
+
+sub _auto_bugtracker {
+  'http://rt.cpan.org/NoAuth/Bugs.html?Dist=' . shift->dist_name;
 }
 
 sub ACTION_testauthor {
